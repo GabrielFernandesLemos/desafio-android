59b6f2f2b8dfe926bbf3c02accfa420d110f60b5
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2021-11-01 17:22:28 -0300

    Moved files to new architecture package

---
+++ app/src/main/java/com/picpay/desafio/android/data/model/User.kt
@@ -0,0 +1,17 @@
+package com.picpay.desafio.android.data.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("img")
+    val img: String,
+    @SerializedName("username")
+    val username: String
+) : Parcelable
--- app/src/main/java/com/picpay/desafio/android/ui/adapter/UserListAdapter.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/adapter/UserListAdapter.kt
@@ -1,9 +1,13 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.UserListDiffCallback
+import com.picpay.desafio.android.UserListItemViewHolder
+import com.picpay.desafio.android.data.model.User
 
 class UserListAdapter : RecyclerView.Adapter<UserListItemViewHolder>() {
 
