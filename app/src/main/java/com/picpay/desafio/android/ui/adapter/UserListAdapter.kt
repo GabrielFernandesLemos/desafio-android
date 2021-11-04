deb5370eb2538c0fa2abb9ab96c5bba1fb5a3c5b
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2021-11-04 03:20:20 -0300

    Remove Files and refactor Adapter

--- app/src/main/java/com/picpay/desafio/android/ui/adapter/UserListAdapter.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/adapter/UserListAdapter.kt
@@ -1,33 +1,26 @@
 package com.picpay.desafio.android.ui.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.picpay.desafio.android.R
-import com.picpay.desafio.android.UserListDiffCallback
-import com.picpay.desafio.android.UserListItemViewHolder
-import com.picpay.desafio.android.data.model.User
+import com.picpay.desafio.android.databinding.ListItemUserBinding
+import com.picpay.desafio.android.ui.entity.User
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
 
-class UserListAdapter : RecyclerView.Adapter<UserListItemViewHolder>() {
+class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserListItemViewHolder>() {
 
     var users = emptyList<User>()
-        set(value) {
-            val result = DiffUtil.calculateDiff(
-                UserListDiffCallback(
-                    field,
-                    value
-                )
-            )
-            result.dispatchUpdatesTo(this)
-            field = value
-        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListItemViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item_user, parent, false)
+        val itemBinding  = ListItemUserBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
 
-        return UserListItemViewHolder(view)
+        return UserListItemViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(holder: UserListItemViewHolder, position: Int) {
@@ -35,4 +28,41 @@ class UserListAdapter : RecyclerView.Adapter<UserListItemViewHolder>() {
     }
 
     override fun getItemCount(): Int = users.size
+
+    class UserListItemViewHolder(
+        private val binding: ListItemUserBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(user: User) {
+            binding.apply {
+                name.text = user.name
+                username.text = user.username
+                progressBar.visibility = View.VISIBLE
+
+                Picasso.get()
+                    .load(user.img)
+                    .error(R.drawable.ic_round_account_circle)
+                    .into(picture, object : Callback {
+                        override fun onSuccess() {
+                            progressBar.visibility = View.GONE
+                        }
+
+                        override fun onError(e: Exception?) {
+                            progressBar.visibility = View.GONE
+                        }
+                    })
+            }
+        }
+    }
+
+    private companion object : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+    }
+
 }
