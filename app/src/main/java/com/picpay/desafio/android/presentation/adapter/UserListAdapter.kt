e7d8ad1e916c54c66bc227a136ecbb78a934f11d
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2021-11-07 22:28:52 -0300

    Update adapter

--- app/src/main/java/com/picpay/desafio/android/presentation/adapter/UserListAdapter.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/adapter/UserListAdapter.kt
@@ -1,20 +1,18 @@
-package com.picpay.desafio.android.ui.adapter
+package com.picpay.desafio.android.presentation.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.databinding.ListItemUserBinding
-import com.picpay.desafio.android.data.model.User
+import com.picpay.desafio.android.domain.model.User
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
-class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserListItemViewHolder>() {
-
-    private val list : MutableList<User> = mutableListOf()
+class UserListAdapter : ListAdapter<User,UserListAdapter.UserListItemViewHolder>(MyDiffUtil()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListItemViewHolder {
         val itemBinding  = ListItemUserBinding.inflate(
@@ -25,20 +23,17 @@ class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserListItemViewHol
     }
 
     override fun onBindViewHolder(holder: UserListItemViewHolder, position: Int) {
-        holder.bind(list[position])
+        holder.bind(getItem(position))
     }
-
-    override fun getItemCount(): Int = list.size
 
     class UserListItemViewHolder(
         private val binding: ListItemUserBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        @SuppressLint("SetTextI18n")
         fun bind(user: User) {
             binding.apply {
                 name.text = user.name
-                username.text = "@"+user.username
+                username.text = "@${user.username}"
                 progressBar.visibility = View.VISIBLE
 
                 Picasso.get()
@@ -57,13 +52,7 @@ class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserListItemViewHol
         }
     }
 
-    fun updateList(list : List<User>){
-        this.list.clear()
-        this.list.addAll(list)
-        notifyDataSetChanged()
-    }
-
-    private companion object : DiffUtil.ItemCallback<User>() {
+    class MyDiffUtil : DiffUtil.ItemCallback<User>() {
         override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
             return oldItem.id == newItem.id
         }
