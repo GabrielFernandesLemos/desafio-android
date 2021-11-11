731a4a6f349df4b7263fd431643777b8c66477a7
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2021-11-11 12:14:02 -0300

    Create UI test

---
+++ app/src/androidTest/java/com/picpay/desafio/android/presentation/fragment/MainFragmentScreenTest.kt
@@ -0,0 +1,46 @@
+package com.picpay.desafio.android.presentation.fragment
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.presentation.MainActivity
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+
+class MainFragmentScreenTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun shouldPassWhenHeaderIsVisible() {
+        val linearLayout = Espresso.onView(
+            Matchers.allOf(
+                withId(R.id.include_header),
+                ViewMatchers.withParent(ViewMatchers.withParent(withId(R.id.container))),
+                isDisplayed()
+            )
+        )
+        linearLayout.check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun shouldPassWhenLogoViewHasVisible() {
+        Espresso.onView(withId(R.id.image_logo))
+            .check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun shouldPassWhenRecyclerIsVisibleAndShowItems() {
+        Espresso.onView(withId(R.id.recyclerView))
+            .perform()
+            .check(matches(isDisplayed()))
+    }
+
+}
